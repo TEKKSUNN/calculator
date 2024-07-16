7df0d37fe7b6a3eaf15db21de6ad0a831f3b8a94
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-07-16 09:36:39 +0800

    Declare & initialize button nodes w/ array ver.

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+const BUTTONS = document.querySelectorAll('button');
+const numberButtons = document.querySelectorAll('button.number');
+const operatorButtons = document.querySelectorAll('button.operator');
+const formatButtons = document.querySelectorAll('button.operator.format');
+const ARRAY_BUTTONS = Array.from(BUTTONS);
+const arrayNumberButtons = Array.from(numberButtons);
+const arrayOperatorButtons = Array.from(operatorButtons);
+const arrayFormatButtons = Array.from(formatButtons);
